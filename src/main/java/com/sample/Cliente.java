1ec9da68d0ff725dec2f298513d96424cf1350b7
cjps91 <carlosjavier.perez@alu.uhu.es>
2018-04-15 18:50:25 +0200

    Añadidos atributos a cliente

--- src/main/java/com/sample/Cliente.java
+++ src/main/java/com/sample/Cliente.java
@@ -1,5 +1,41 @@
 package com.sample;
+import java.util.Date;
 
 public class Cliente {
+	
+	private int id;
+	private String name;
+	private Date ultimafechaacceso;
+	
+	public int get_id()
+	{
+		return id;
+	}
+	
+	public void set_id(int id)
+	{
+		this.id=id;
+	}
+	
+	public String get_name()
+	{
+		return name;
+	}
 
+	public void set_name(String name)
+	{
+		this.name=name;
+	}
+	
+	public Date get_date()
+	{
+		return ultimafechaacceso;
+	}
+	
+	public void set_date(Date fecha)
+	{
+		this.ultimafechaacceso=fecha;
+	}
+	
 }
+
